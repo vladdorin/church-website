600dbf1f794b7657d9e512116c31346c5b73f137
vladvoica <vlad.voica@fanduel.com>
2026-05-12 12:40:52 +0300

    test

--- app/api/form/route.ts
+++ app/api/form/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 export async function POST(req: Request) {
   try {
